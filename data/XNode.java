f8157ab1b774fa1b7baa9f544b61247bd3cc9a06
Michael Carleton <micycle1@hotmail.co.uk>
2021-08-13 12:29:02 +0100

    source clean up

--- data/XNode.java
+++ data/XNode.java
@@ -2,21 +2,23 @@ package data;
 
 /**
  * An X node stores a segment end point
+ * 
  * @author Tyler Chenhall
  */
 public class XNode extends Node {
-    private Point data;
-    
-    public XNode(Point p) {
-        super();
-        data = p;
-    }
-    
-    /**
-     * Return the Point contained in the Node
-     * @return The Point data
-     */
-    public Point getData() {
-        return data;
-    }
+	private Point data;
+
+	public XNode(Point p) {
+		super();
+		data = p;
+	}
+
+	/**
+	 * Return the Point contained in the Node
+	 * 
+	 * @return The Point data
+	 */
+	public Point getData() {
+		return data;
+	}
 }
--- data/YNode.java
+++ data/YNode.java
@@ -2,21 +2,23 @@ package data;
 
 /**
  * A y-node stores a segment
+ * 
  * @author Tyler Chenhall
  */
-public class YNode extends Node{
-    private Segment data;
-    
-    public YNode(Segment s) {
-        super();
-        data = s;
-    }
-    
-    /**
-     * Return the segment data held by this Node
-     * @return the segment data
-     */
-    public Segment getData() {
-        return data;
-    }
+public class YNode extends Node {
+	private Segment data;
+
+	public YNode(Segment s) {
+		super();
+		data = s;
+	}
+
+	/**
+	 * Return the segment data held by this Node
+	 * 
+	 * @return the segment data
+	 */
+	public Segment getData() {
+		return data;
+	}
 }
